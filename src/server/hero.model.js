05a6c23a66796c4c178bb15176d5dfe4c4a45aef
azadehkhojandi <azadehkhojandi@gmail.com>
2017-11-15 11:45:48 +1100

    remove id unique from schema - temp fix

--- src/server/hero.model.js
+++ src/server/hero.model.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 
 const heroSchema = new Schema(
   {
-    id: { type: Number, required: true, unique: true },
+    id: { type: Number, required: true },
     name: String,
     saying: String
   },
